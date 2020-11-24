ff1e9d5357287c374efe8629550c7d2faaf06821
Med Ghaith Sellami <medghaith.sellami@gmail.com>
2020-11-24 10:14:03 +0100

    fix building error TS2339 Property does not exist

--- src/lib/adapter.ts
+++ src/lib/adapter.ts
@@ -170,7 +170,7 @@ export class MongoAdapter implements Adapter {
     fieldIndex: number,
     ...fieldValues: string[]
   ) {
-    const line = {};
+    const line: any = {};
 
     line.ptype = ptype;
 
